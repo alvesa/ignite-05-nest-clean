f4fe19adec7d18b72c07674de82f30865d9d0411
Andre Alves <andresistinfo@gmail.com>
2023-11-28 06:45:44 +0000

    Initializing jwt authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   controllers: [CreateAccountController],
@@ -11,6 +12,7 @@ import { envSchema } from './env'
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
   ],
   providers: [PrismaService],
 })
