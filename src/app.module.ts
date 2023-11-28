d697094ccc6bb525f1287682ae6a32a805829fc5
Andre Alves <andresistinfo@gmail.com>
2023-11-28 06:31:08 +0000

    Adding configuration module and env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env'
 
 @Module({
   controllers: [CreateAccountController],
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
