072d0a22b992dceabfaa9b55945141e14e941d32
Andre Alves <andresistinfo@gmail.com>
2023-11-29 05:36:50 +0000

    Validating auth guards and creating jwt strategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,15 @@ import { CreateAccountController } from './controllers/create-account.controller
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
-import { AuthenticateController } from './controllers/authenticate-controller'
+import { AuthenticateController } from './controllers/authenticate.controller'
+import { CreateQuestionCrontroller } from './controllers/create-question.controller'
 
 @Module({
-  controllers: [CreateAccountController, AuthenticateController],
+  controllers: [
+    CreateAccountController,
+    AuthenticateController,
+    CreateQuestionCrontroller,
+  ],
   imports: [
     ConfigModule.forRoot({
       validate: (env) => envSchema.parse(env),
